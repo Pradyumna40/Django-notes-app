9e30646967e39e8abd66fe595764867375c3de31
Pradyumna40 <arakeripradyumna14@gmail.com>
2026-02-05 17:53:53 +0530

    Css linking and whitenoise

--- config/settings.py
+++ config/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -120,3 +121,5 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_DIRS = [
     BASE_DIR / 'notes' / 'static',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
